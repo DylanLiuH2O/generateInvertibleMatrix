a2666ad8434d5ca915e059a6464c8eb3d04f534a
Dylan <913464928@qq.com>
2021-06-15 23:32:28 +0800

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # generateInvertibleMatrix
 随机可逆矩阵生成小工具（a tool of generating random invertible matrix）
+
+## 算法  
+
+本质上是一定迭代次数的随机初等行变换，由随机数决定执行何种初等行变换，最后得到可逆矩阵。  
+
+## 用法  
+
+编译源代码，`g++ ./genInvertibleMat.cc -o genInver`。  
+命令格式: `./genInvertibaleMat <iterations> <order> <filename> [int/double]`。  
+`iteration`是迭代次数，即执行初等行变换的次数，次数越多一般值变化越大。  
+`order`是矩阵阶数。
+`filename`是文件名，不带路径则生成于当前目录，可以使用其他位置的路径。  
+`int/double`是可选项，用来决定倍加变换和倍乘变换的因子是整数还是浮点数。  
+
---
+++ genInvertibaleMat.cc
@@ -0,0 +1,172 @@
+/*
+ * @Author: dylan
+ * @Date: 2021-06-14 13:13:09
+ * @LastEditTime: 2021-06-14 16:28:15
+ * @LastEditors: dylan
+ * @Description: generate an invertibale n-order matrix
+ * @FilePath: /paraMatInver/tools/genInvertibaleMat.cc
+ */
+#include <iostream>
+#include <fstream>
+#include <exception>
+#include <algorithm>
+#include <random>
+#include <vector>
+
+using namespace std;
+
+template<typename T>
+void rowSwitch(vector<vector<T>> &mat, int row1, int row2);
+template<typename T, typename U>
+void rowMultiply(vector<vector<T>> &mat, int row1, U factor);
+template<typename T, typename U>
+void rowAddition(vector<vector<T>> &mat, int row1, int row2, U times);
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4 && argc != 5) {
+        cerr << "usage: " << argv[0] << " <iterations> <order> <filename> [int/double] \n";
+        exit(1);
+    }
+
+    int iterations;
+    int order;
+    string filename   = argv[3];
+    string numberType = (argc == 5) ? argv[4] : "";
+
+    try {
+        iterations  = stoi(argv[1]);
+        order       = stoi(argv[2]);
+    } catch (invalid_argument) {
+        cerr << "invalid argument\n";
+        exit(1);
+    } catch (out_of_range) {
+        cerr << "out of range\n";
+        exit(1);
+    }
+
+    if (argc == 5 && numberType != "int" && numberType != "double") {
+        cerr << "number type error\n";
+        exit(1);
+    }
+
+#if true
+    cout << "argc: " << argc << "\n";
+    cout << "iterations: " << iterations << "\n";
+    cout << "order: " << order << "\n";
+    cout << "filename: " << filename << "\n";
+    if (numberType != "") {
+        cout << "numberType: " << numberType << "\n";
+        // cout << (numberType == "int") << " " << (numberType == "double") << "\n";
+    }
+#endif
+
+    /* create an identity matrix */
+    vector<vector<double>> mat(order, vector<double>(order, 0));
+    for (int i = 0; i < order; i++) {
+        mat[i][i] = 1.0;
+    }
+
+    enum class rowTransType {
+        SWITCH   = 0, 
+        MULTIPLY = 1, 
+        ADDITION = 2
+    };
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int>   randomType(0, 2);
+    uniform_int_distribution<int>   randomRow(0, order-1);
+    uniform_int_distribution<int>   randomFactorInt(1, 5);
+    uniform_int_distribution<int>   randomTimesInt(1, 10);
+    uniform_real_distribution<double>  randomFactorDouble(1, 5);
+    uniform_real_distribution<double>  randomTimesDouble(1, 10);
+
+    for (int i = 0; i < iterations; i++) {
+        rowTransType type = (rowTransType)randomType(gen);
+        int row1     = randomRow(gen);
+        int row2     = randomRow(gen);
+        while ((row2 = randomRow(gen)) == row1) { }
+
+        int timesInt        = randomTimesInt(gen);
+        int factorInt       = randomFactorInt(gen);
+        double timesDouble  = randomTimesInt(gen);
+        double factorDouble = randomFactorDouble(gen);;
+        switch (type) {
+            case rowTransType::SWITCH:
+                rowSwitch(mat, row1, row2);
+                break;
+            case rowTransType::MULTIPLY:
+                if (numberType == "int"){
+                    rowMultiply(mat, row1, factorInt);
+                } else {
+                    rowMultiply(mat, row1, factorDouble);
+                }
+                break;
+            case rowTransType::ADDITION:
+                if (numberType == "int"){
+                    rowAddition(mat, row1, row2, timesInt);
+                } else {
+                    rowAddition(mat, row1, row2, timesDouble);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    /* open file */
+    ofstream out;
+    out.open(filename);
+    if (!out) {
+        cerr << "file \"" << filename << "\" open error\n";
+    }
+
+    /* output matrix to file */
+    // out << order << " " << order << " " << order << "\n";
+    out << order << "\n";
+    for (int i = 0; i < order; i++) {
+        for (int j = 0; j < order; j++) {
+            if (mat[i][j] != 0) {
+                out << i << " " << j << " " << mat[i][j] << "\n";
+            }
+        }
+    }
+
+    /* close file */
+    out.close();
+
+    return 0;
+}
+
+template<typename T>
+void rowSwitch(vector<vector<T>> &mat, int row1, int row2)
+{
+    const int order = mat.size();
+
+    for (int j = 0; j < order; j++) {
+        swap(mat[row1][j], mat[row2][j]);
+    }
+}
+
+template<typename T, typename U>
+void rowMultiply(vector<vector<T>> &mat, int row, U factor)
+{
+    if (factor == 0) {
+        return;
+    }
+    const int order = mat.size();
+
+    for (int j = 0; j < order; j++) {
+        mat[row][j] *= factor;
+    }
+}
+
+template<typename T, typename U>
+void rowAddition(vector<vector<T>> &mat, int row1, int row2, U times)
+{
+    const int order = mat.size();
+
+    for (int j = 0; j < order; j++) {
+        mat[row1][j] += mat[row2][j] * times;
+    }
+}
